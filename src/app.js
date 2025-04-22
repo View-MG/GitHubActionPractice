c6081138bd96453abf6878db2d72b7f9931aa3f3
Magnet <phatthansn.charoenphol@gmail.com>
2025-04-22 16:15:05 +0700

    update app.js and app.test.js

--- src/app.js
+++ src/app.js
@@ -5,14 +5,17 @@ const dayOfTheWeek = (date = new Date()) => {
 }
 
 const monthOfTheYear = (date = new Date()) => {
-    const month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    const month = ['January', 'Februrary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
     return month[date.getMonth()];
-  }
-  
+
+}
 
 try {
     document.getElementById('day').innerText = dayOfTheWeek();
+    document.getElementById('month').innerText = monthOfTheYear();
 } catch(err) {}
 
 
 exports.dayOfTheWeek = dayOfTheWeek;
+exports.monthOfTheYear = monthOfTheYear;
